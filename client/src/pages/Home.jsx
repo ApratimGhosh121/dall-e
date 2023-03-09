ad6b18aa8f3363cc4b8437365fe464b1a0083c14
Apratim <apratimghosh76351@gmail.com>
2023-03-09 17:15:17 +0530

    Changed localhost to Render App

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-40dz.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'content-type': 'application/json',
